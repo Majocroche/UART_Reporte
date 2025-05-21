8abd36297152cfb4c6ec214b2d418b055b1f8e3a
Armando <pabloarmando.macbeath@gmail.com>
2025-05-20 19:56:56 -0600

    Update and rename LED_button.ino to uart.ino

---
+++ uart.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Hola");
+  delay(5000);
+  Serial.println("Somos:");
+  delay(5000);
+  Serial.println("MácBet");
+  delay(5000);
+  Serial.println("A01735082");
+  delay(5000);
+  Serial.println("Majo");
+  delay(5000);
+  Serial.println("A01734561");
+  delay(5000);
+  Serial.println("Xime");
+  delay(5000);
+
+}
